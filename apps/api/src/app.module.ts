b01c7343e00994a3c12d476b50373b509ae1fa4c
August McAllister <august.chm@gmail.com>
2024-04-06 11:38:14 -0500

    chore: all together now (app module complete)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './routes/user/user.module';
 import configuration from './config/configuration';
+import * as schema from './drizzle/schema';
+import { DrizzlePostgresModule } from '@knaadh/nestjs-drizzle-postgres';
+import { ThreadsModule } from './routes/threads/threads.module';
+import { LoggerMiddleware } from './common/middleware/LoggerMiddleware';
+import { AuthModule } from './routes/auth/auth.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: '.env.api',
             isGlobal: true,
+            cache: true,
             load: [configuration],
         }),
+        UserModule,
+        DrizzlePostgresModule.registerAsync({
+            tag: 'DB',
+            useFactory: async () => ({
+                postgres: {
+                    url: process.env.DATABASE_URL!,
+                },
+                config: { schema },
+            }),
+        }),
+        ThreadsModule,
+        AuthModule,
     ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
