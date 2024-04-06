5e855b092cebaa7f2d9558d915f1700a71df8538
August McAllister <august.chm@gmail.com>
2024-04-06 13:52:11 -0500

    feat: first query client

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,19 @@
 import './globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
+    const queryClient = new QueryClient();
+
     return (
-        <main>
-            <h1>YO</h1>
-        </main>
+        <QueryClientProvider client={queryClient}>
+            <main>
+                <div>
+                    <h1>YO</h1>
+                </div>
+            </main>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
