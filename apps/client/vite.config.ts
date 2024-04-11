ea6b311ce6de36ab54590ba7b06afb75fd64f571
August McAllister <august.chm@gmail.com>
2024-04-11 11:50:23 -0500

    feat: added dependencies / utilities for ts and tanstack router

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
+import TsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), TanStackRouterVite(), TsConfigPaths()],
 });
