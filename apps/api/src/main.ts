a45dad2b52187c3fd182e4428eddbedc811ff861
August McAllister <august.chm@gmail.com>
2024-04-11 11:46:02 -0500

    feat: implemented cookie parser and cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+const appConfig = [cookieParser()];
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(appConfig);
+    app.enableCors({
+        origin: [process.env.CLIENT_URL!, 'http://localhost:3000'],
+        credentials: true,
+    });
     await app.listen(3000);
 }
 bootstrap();
