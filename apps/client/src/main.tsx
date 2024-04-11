517791ea200319c7aa06bd37a5796ddcdfa77562
August McAllister <august.chm@gmail.com>
2024-04-11 11:52:39 -0500

    feat: new main.tsx and initial route tree
    Route tree will regenerate with every new build

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import './styles/globals.scss';
+
+import { routeTree } from './routeTree.gen.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>,
 );
