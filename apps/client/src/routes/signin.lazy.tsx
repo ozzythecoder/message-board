d064421fcf79be9b5477ef36f0a14e8a7cc5cf1b
August McAllister <august.chm@gmail.com>
2024-06-12 11:15:04 -0500

    chore: sign in page renamed properly

--- apps/client/src/routes/signin.lazy.tsx
+++ apps/client/src/routes/signin.lazy.tsx
@@ -1,11 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 import SignInForm from 'src/features/auth/components/SignInForm';
 
-export const Route = createFileRoute('/about')({
-    component: () => <About />,
+export const Route = createFileRoute('/signin')({
+    component: () => <SignIn />,
 });
 
-function About() {
+function SignIn() {
     return (
         <div>
             <SignInForm />
