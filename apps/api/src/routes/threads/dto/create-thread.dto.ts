d8fed93046b12173e0f8d10bf977a4cd071d7c1d
August McAllister <august.chm@gmail.com>
2024-06-12 11:01:34 -0500

    fix: added missing title property to thread schema

--- apps/api/src/routes/threads/dto/create-thread.dto.ts
+++ apps/api/src/routes/threads/dto/create-thread.dto.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const createThreadSchema = z.object({
+    title: z.string().max(300),
     body: z.string().max(5000),
     topicID: z.string().uuid(),
     authorID: z.string().uuid(),
