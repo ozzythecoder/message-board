fcc17c8a1d7a0e7da88369dd6843d870b224e030
August McAllister <august.chm@gmail.com>
2024-06-12 11:12:45 -0500

    feat: topic list

---
+++ apps/client/src/features/topics/components/TopicsList.tsx
@@ -0,0 +1,20 @@
+import { Loading } from 'src/components/ui';
+import { useTopics } from '../api';
+import { TopicPreview } from './TopicPreview';
+
+export function TopicsList() {
+    const { data: topics, isError, isPending } = useTopics();
+
+    if (isPending) return <Loading />;
+    if (isError) return <h1>Error</h1>;
+
+    return (
+        <div>
+            <ul>
+                {topics.map((t) => (
+                    <TopicPreview key={t.id} topic={t} />
+                ))}
+            </ul>
+        </div>
+    );
+}
