fbd41be7de7fb38e8779557dff408b1f27539c9f
August McAllister <august.chm@gmail.com>
2024-06-12 10:55:39 -0500

    feat: /user endpoints now use auth guard

--- apps/api/src/routes/user/user.controller.ts
+++ apps/api/src/routes/user/user.controller.ts
@@ -5,21 +5,25 @@ import {
     Param,
     ParseUUIDPipe,
     Post,
+    UseGuards,
     UsePipes,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO, createUserSchema } from './dto/create-user.dto';
 import { ZodValidationPipe } from 'src/common/utils/ZodValidationPipe';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     find(@Param('id', ParseUUIDPipe) id: string) {
         return this.userService.find(id);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     findAll() {
         return this.userService.findAll();
