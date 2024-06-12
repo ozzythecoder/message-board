ea00279d56fd906ef2a39145b5bd6bb97bb80ee9
August McAllister <august.chm@gmail.com>
2024-06-12 11:12:21 -0500

    feat: topics api functions updated

--- apps/client/src/features/topics/api/index.ts
+++ apps/client/src/features/topics/api/index.ts
@@ -1,5 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
 import api from 'src/config/api';
+import type { QueryKeyObject, Topic } from 'src/types';
 
-export const getTopics = async () => {
-    return await api.get('/topics');
+const getTopics = async () => {
+    const result = await api.get<Topic[]>('/topics');
+    return result.data;
+};
+
+export const useTopics = () => {
+    return useQuery({ queryKey: ['topics'], queryFn: getTopics });
+};
+
+const getOneTopic = async (q: QueryKeyObject<string>) => {
+    const [_, topicID] = q.queryKey;
+    const result = await api.get<Topic>(`/topics/${topicID}`);
+    return result.data;
+};
+
+export const useOneTopic = (topicID: string) => {
+    return useQuery({ queryKey: ['topic', topicID], queryFn: getOneTopic });
 };
