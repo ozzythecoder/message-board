1fb8418ef1f3f00a249efa6d6635c5d8ec185602
August McAllister <august.chm@gmail.com>
2024-06-12 10:56:11 -0500

    refactor: clearer logic flow in user service

--- apps/api/src/routes/user/user.service.ts
+++ apps/api/src/routes/user/user.service.ts
@@ -5,7 +5,7 @@ import {
     UnauthorizedException,
 } from '@nestjs/common';
 import { hash } from 'bcryptjs';
-import { eq } from 'drizzle-orm';
+import { eq, or } from 'drizzle-orm';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as schema from 'src/drizzle/schema';
 import { CreateUserDTO } from './dto/create-user.dto';
@@ -48,23 +48,34 @@ export class UserService {
         userDTO: CreateUserDTO,
     ): Promise<Omit<schema.User, 'password'>> {
         const existingUser = await this.db.query.user.findFirst({
-            where: eq(schema.user.email, userDTO.email),
+            where: or(
+                eq(schema.user.email, userDTO.email),
+                eq(schema.user.username, userDTO.username),
+            ),
         });
 
         if (existingUser)
-            throw new ConflictException('Duplicated email detected.');
+            throw new ConflictException(
+                'Duplicate email or username detected.',
+            );
 
         const hashedPassword = await hash(userDTO.password, 10);
 
-        const newUsers = await this.db
+        const newUser: Omit<schema.User, 'id'> = {
+            username: userDTO.username,
+            email: userDTO.email,
+            firstName: userDTO.firstName ?? null,
+            lastName: userDTO.lastName ?? null,
+            dateJoined: new Date(Date.now()).toISOString(),
+            password: hashedPassword,
+        };
+
+        const newUserResponse = await this.db
             .insert(schema.user)
-            .values({
-                ...userDTO,
-                password: hashedPassword,
-            })
+            .values(newUser)
             .returning();
 
-        const { password: _, ...result } = newUsers[0];
+        const { password: _, ...result } = newUserResponse[0];
         return result;
     }
 }
