47900d27ea197cd3ce9ba3a4101162c7b19f9d56
August McAllister <august.chm@gmail.com>
2024-06-12 10:52:49 -0500

    chore: updated logger middleware to use network objects

--- apps/api/src/common/middleware/LoggerMiddleware.ts
+++ apps/api/src/common/middleware/LoggerMiddleware.ts
@@ -2,8 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    use(req: any, _res: any, next: (error?: any) => void) {
-        console.log('Request...\n');
+    use(req: Request, res: Response, next: (error?: any) => void) {
         next();
     }
 }
