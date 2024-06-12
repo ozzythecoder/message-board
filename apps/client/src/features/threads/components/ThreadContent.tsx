9cf26cdceff81c63464fcc76ef996f3331c712cd
August McAllister <august.chm@gmail.com>
2024-06-12 15:18:06 -0500

    refactor: thread content using new Card component and user sidebar

--- apps/client/src/features/threads/components/ThreadContent.tsx
+++ apps/client/src/features/threads/components/ThreadContent.tsx
@@ -1,22 +1,24 @@
 import { ApiThreadResponse } from 'src/types';
 import style from './threadContent.module.scss';
-import { formatDate } from 'src/lib/utils';
+import { UserSidebar } from 'src/features/users';
+import { Card } from 'src/components/ui';
 
 export function ThreadContent({ data }: { data: ApiThreadResponse }) {
-    const formattedDate = formatDate(data.thread.createdAt);
-
     return (
-        <div className={style.thread_content}>
-            <h2>{data.thread.title}</h2>
-            <div className={style.post_content}>
-                <div className={style.user_info}>
-                    <p>{data.user.username}</p>
-                    <p className={style.date_posted}>{formattedDate}</p>
-                </div>
-                <div className={style.post_body}>
-                    <p>{data.thread.body}</p>
-                </div>
+        <Card
+            renderSidebarContent={() => (
+                <UserSidebar
+                    username={data.user.username}
+                    datePosted={data.thread.createdAt}
+                />
+            )}
+            title={data.thread.title}
+            border="thin"
+            className={style.thread_content}
+        >
+            <div>
+                <p>{data.thread.body}</p>
             </div>
-        </div>
+        </Card>
     );
 }
