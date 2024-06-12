02b35ccc28371f774cdd066a4c8de1f44afc2042
August McAllister <august.chm@gmail.com>
2024-06-12 11:14:49 -0500

    feat: updated root layout
    Includes conditional rendering for dev tools

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import config from 'src/config';
 import styles from './root.module.scss';
 
 export const Route = createRootRoute({
@@ -8,12 +10,19 @@ export const Route = createRootRoute({
             <h1 className={styles.heading}>Message Board</h1>
             <div className={styles.nav}>
                 <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
+                <Link to="/signin">Sign In</Link>
+                <Link to="/topics">Topics</Link>
+                <Link to="/colors">Colors</Link>
             </div>
             <main>
                 <Outlet />
             </main>
-            <TanStackRouterDevtools />
+            {config.DEV ? (
+                <>
+                    <TanStackRouterDevtools />
+                    <ReactQueryDevtools />
+                </>
+            ) : null}
         </div>
     ),
 });
