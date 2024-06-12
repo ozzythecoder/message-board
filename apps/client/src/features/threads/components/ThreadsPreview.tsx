d4fd58a6d5729766a94d0285bdd3b0520f2a503e
August McAllister <august.chm@gmail.com>
2024-06-12 15:17:42 -0500

    refactor: thread preview using new Card component

--- apps/client/src/features/threads/components/ThreadsPreview.tsx
+++ apps/client/src/features/threads/components/ThreadsPreview.tsx
@@ -1,13 +1,28 @@
 import { Link } from '@tanstack/react-router';
+import { Card } from 'src/components/ui';
 import { ApiThreadResponse } from 'src/types';
+import { formatDate } from 'src/lib/utils';
+import { UserSidebar } from 'src/features/users';
 
 export function ThreadPreview({ data }: { data: ApiThreadResponse }) {
     return (
         <Link to="/threads/$threadID" params={{ threadID: data.thread.id }}>
-            <li key={data.thread.id}>
-                <h3>{data.thread.title}</h3>
-                <p>{data.thread.body.slice(0, 100)}...</p>
-            </li>
+            <Card
+                border="thin"
+                renderSidebarContent={() => (
+                    <UserSidebar
+                        username={data.user.username}
+                        datePosted={formatDate(data.thread.createdAt)}
+                    />
+                )}
+            >
+                <li key={data.thread.id}>
+                    <h3>{data.thread.title}</h3>
+                    <p className="no_weight text">
+                        {data.thread.body.slice(0, 100)}...
+                    </p>
+                </li>
+            </Card>
         </Link>
     );
 }
