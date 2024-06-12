7dd0cdae37856e6269d9601b92dc1a3ba9f12774
August McAllister <august.chm@gmail.com>
2024-06-12 11:08:46 -0500

    chore: barrel file

---
+++ apps/client/src/components/ui/index.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
+export * from './Loading';
+export * from './Button';
+export * from './Input';
+export * from './Textarea';
+export * from './Card';
